cdece8a97f44eaceb90d369c1b0efd5d4c127eb6
anjo <anjojosephapj@gmail.com>
2023-12-20 12:39:13 +1100

    commit code

--- dbConnection.js
+++ dbConnection.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://anjo:admin@cluster0.klae78e.mongodb.net/?retryWrites=true&w=majority"
+const uri = "mongodb+srv://anjo:admin@cluster0.ykgewaw.mongodb.net/?retryWrites=true&w=majority"
 
 const client = new MongoClient(uri, {
     serverApi: {
